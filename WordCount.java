fc34eaa3b2d7e4806669b659d6a4c185a3533436
Frostyturtle <Frostyturtle@users.noreply.github.com>
2017-05-12 14:33:44 -0500

    Cleaned up code
    Added helper methods and error checking for file name

--- WordCount.java
+++ WordCount.java
@@ -5,17 +5,8 @@ import java.util.Scanner;
 
 public class WordCount {
   
-  public static void main (String args[]) throws Exception {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter a file name");
-    String fileName = sc.nextLine();
-    
-    //Input file
-    File file = new File(fileName);
-    Scanner fileIn = new Scanner(file);
-    sc.close();
-    
-    //Count words
+  //Store counts of words in HashMap
+  public static Map<String, Integer> countWords(Scanner fileIn) {
     HashMap<String, Integer> counts = new HashMap<String, Integer>();
     while (fileIn.hasNext()) {
       String cur = fileIn.next();
@@ -24,12 +15,39 @@ public class WordCount {
       else
         counts.put(cur, counts.get(cur)+1);
     }
-    fileIn.close();
-    
+    return counts;
+  }
+  
+  // Iterate through map and print counts
+  public static void printResults(Map<String, Integer> counts) {
     for (Map.Entry<String, Integer> entry : counts.entrySet()) {
       String word = entry.getKey();
       Integer occurrences = entry.getValue();
       System.out.println(word + ", " + occurrences);
+    }
+  }
+  
+  public static void main (String args[]) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a file name");
+    String fileName = sc.nextLine();
+    
+    //Input file
+    File file = new File(fileName);
+    sc.close();
+    //Make sure file is valid
+    if (file.exists() && !file.isDirectory()) {
+      Scanner fileIn = new Scanner(file);
+      
+      //Count words
+      Map<String, Integer> counts = countWords(fileIn);
+      fileIn.close();
+      
+      //Print out counts
+      printResults(counts);
+    }
+    else {
+      System.out.println(fileName + " is not a valid file. Exiting.");
     }
   }
 }
