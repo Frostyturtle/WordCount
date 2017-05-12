88320d457da5b2ea378e26fb54547881d099bf62
Frostyturtle <Frostyturtle@users.noreply.github.com>
2017-05-12 15:12:12 -0500

    Added functionality
    Punctuation removed from words
    Added optional "-i" flag for input (Counts words disregarding case differences)

--- WordCount.java
+++ WordCount.java
@@ -4,12 +4,31 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class WordCount {
+  static boolean caseInsensitive = false;
+  
+  //Checks if input line had " -i " anywhere
+  public static void checkFlags(String[] input) {
+    for (int i = 1; i < input.length; i++) {
+      if (input[i].equals("-i"))
+        caseInsensitive = true;
+    }
+  }
+  
+  public static String removePunctuation(String word) {
+    return word.replaceAll("[^a-zA-Z ]", "");
+    //return word.replaceAll("\\p{P}", "");
+  }
   
   //Store counts of words in HashMap
   public static Map<String, Integer> countWords(Scanner fileIn) {
     HashMap<String, Integer> counts = new HashMap<String, Integer>();
     while (fileIn.hasNext()) {
-      String cur = fileIn.next();
+      String cur = removePunctuation(fileIn.next());
+      
+      //If user passed in "-i" flag, convert all letters to lowercase
+      if (caseInsensitive)
+        cur = cur.toLowerCase();
+      
       if (!counts.containsKey(cur))
         counts.put(cur, 1);
       else
@@ -30,7 +49,10 @@ public class WordCount {
   public static void main (String args[]) throws Exception {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter a file name");
-    String fileName = sc.nextLine();
+    String line = sc.nextLine();
+    String[] input = line.split("\\s+");
+    String fileName = input[0];
+    checkFlags(input);
     
     //Input file
     File file = new File(fileName);
